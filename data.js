60d1c3bb67d8f0f5bdf11b62a4d17d3d91546239
JHONATTAN CUNHA <60943856+JC2106@users.noreply.github.com>
2021-07-07 17:29:37 -0300

    commit

--- data.js
+++ data.js
@@ -3,7 +3,6 @@ export const data = [
       "id": 1,
       "category": "burger",
       "name": "Cheese Burger",
-      "ingredients": ["pão", "molho da casa", "hamburger 200g", "alface", "tomate", "queijo"],
       "price": 15.50,
       "sale": true,
       "image": "/assets/images/burger1.png"
@@ -12,7 +11,6 @@ export const data = [
       "id": 2,
       "category": "burger",
       "name": "Double Cheese",
-      "ingredients": ["pão", "molho da casa", "hamburger 200g", "alface", "tomate", "2x queijo"],
       "price": 17.00,
       "sale": true,
       "image": "/assets/images/burger2.png"
@@ -21,7 +19,6 @@ export const data = [
       "id": 3,
       "category": "burger",
       "name": "Double Burger",
-      "ingredients": ["pão", "molho da casa", "2x hamburger 200g", "alface", "tomate", "2x queijo"],
       "price": 25.00,
       "sale": false,
       "image": "/assets/images/burger3.png"
@@ -30,7 +27,6 @@ export const data = [
       "id": 4,
       "category": "burger",
       "name": "Triple Burger",
-      "ingredients": ["pão", "molho da casa", "3x hamburger 200g", "alface", "tomate", "3x queijo"],
       "price": 28.00,
       "sale": false,
       "image": "/assets/images/burger4.png"
@@ -39,7 +35,6 @@ export const data = [
       "id": 5,
       "category": "burger",
       "name": "Bacon Burger",
-      "ingredients": ["pão", "molho barbecue", "hamburger 200g", "cebola caramelizada", "bacon caramelizado", "2x queijo"],
       "price": 25.00,
       "sale": false,
       "image": "/assets/images/burger7.png"
@@ -48,7 +43,6 @@ export const data = [
       "id": 6,
       "category": "pizza",
       "name": "Calabresa",
-      "ingredients": ["molho de tomate", "queijo", "calabresa", "azeitonas", "oregano"],
       "price": [20.00, 30.00],
       "sale": true,
       "image": "/assets/images/pizza1.png"
@@ -57,7 +51,6 @@ export const data = [
       "id": 7,
       "category": "pizza",
       "name": "4 Queijos",
-      "ingredients": ["molho de tomate", "muçarela", "provolone", "requeijão cremoso", "parmesão", "azeitonas", "oregano"],
       "price": [24.00, 40.00],
       "sale": false,
       "image": "/assets/images/pizza1.png"
@@ -66,7 +59,6 @@ export const data = [
       "id": 8,
       "category": "pizza",
       "name": "Lombo",
-      "ingredients": ["molho de tomate", "muçarela", "lombo", "azeitonas", "oregano"],
       "price": [26.00, 42.00],
       "sale": false,
       "image": "/assets/images/pizza2.png"
@@ -75,7 +67,6 @@ export const data = [
       "id": 9,
       "category": "burger",
       "name": "Australian Burger",
-      "ingredients": ["pão australiano", "maionese", "hamburger 200g", "cebola caramelizada", "bacon caramelizado", "ovo", "queijo"],
       "price": 25.00,
       "sale": false,
       "image": "/assets/images/burger6.png"
@@ -84,7 +75,6 @@ export const data = [
       "id": 10,
       "category": "pizza",
       "name": "Portuguesa",
-      "ingredients": ["molho de tomate", "muçarela", "presunto", "ovo", "azeitonas", "oregano"],
       "price": [25.00, 44.00],
       "sale": false,
       "image": "/assets/images/pizza3.png"
@@ -92,8 +82,7 @@ export const data = [
     {
       "id": 11,
       "category": "pizza",
-      "name": "Portuguesa",
-      "ingredients": ["molho de tomate", "muçarela", "presunto", "ovo", "azeitonas", "oregano"],
+      "name": "Moda do Chef",
       "price": [25.00, 44.00],
       "sale": false,
       "image": "/assets/images/pizza4.png"
@@ -101,8 +90,7 @@ export const data = [
     {
       "id": 12,
       "category": "pizza",
-      "name": "Portuguesa",
-      "ingredients": ["molho de tomate", "muçarela", "presunto", "ovo", "azeitonas", "oregano"],
+      "name": "Moda da Casa",
       "price": [25.00, 44.00],
       "sale": false,
       "image": "/assets/images/pizza5.png"
@@ -111,7 +99,6 @@ export const data = [
       "id": 13,
       "category": "taco",
       "name": "Tacos com chili mexicano",
-      "ingredients": ["molho de tomate", "muçarela", "presunto", "ovo", "azeitonas", "oregano"],
       "price": 10,
       "sale": false,
       "image": "/assets/images/taco1.png"
@@ -120,7 +107,6 @@ export const data = [
       "id": 14,
       "category": "taco",
       "name": "Tacos de suadero",
-      "ingredients": ["molho de tomate", "muçarela", "presunto", "ovo", "azeitonas", "oregano"],
       "price": 12,
       "sale": false,
       "image": "/assets/images/taco2.png"
@@ -129,7 +115,6 @@ export const data = [
       "id": 15,
       "category": "taco",
       "name": "Tacos mexicanos de frango",
-      "ingredients": ["molho de tomate", "muçarela", "presunto", "ovo", "azeitonas", "oregano"],
       "price": 15,
       "sale": false,
       "image": "/assets/images/taco3.png"
@@ -138,7 +123,6 @@ export const data = [
       "id": 16,
       "category": "taco",
       "name": "Tacos mexicanos de carne",
-      "ingredients": ["molho de tomate", "muçarela", "presunto", "ovo", "azeitonas", "oregano"],
       "price": 18,
       "sale": false,
       "image": "/assets/images/taco4.png"
@@ -147,7 +131,6 @@ export const data = [
       "id": 17,
       "category": "snack",
       "name": "Batata Frita",
-      "ingredients": ["batata frita"],
       "price": [10.00, 20.00],
       "sale": false,
       "image": "/assets/images/snack1.png"
@@ -156,7 +139,6 @@ export const data = [
       "id": 18,
       "category": "snack",
       "name": "Onion Rings",
-      "ingredients": ["batata frita"],
       "price": [12.00, 25.00],
       "sale": false,
       "image": "/assets/images/snack2.png"
@@ -165,7 +147,6 @@ export const data = [
       "id": 19,
       "category": "hotdog",
       "name": "Hotdog pequeno",
-      "ingredients": ["salsicha", "pão", "molho"],
       "price": 8,
       "sale": false,
       "image": "/assets/images/hotdog1.png"
@@ -174,7 +155,6 @@ export const data = [
       "id": 20,
       "category": "hotdog",
       "name": "Hotdog medio",
-      "ingredients": ["salsicha", "pão", "molho"],
       "price": 12,
       "sale": false,
       "image": "/assets/images/hotdog2.png"
@@ -183,7 +163,6 @@ export const data = [
       "id": 21,
       "category": "hotdog",
       "name": "Hotdog grande",
-      "ingredients": ["salsicha", "pão", "molho"],
       "price": 18,
       "sale": false,
       "image": "/assets/images/hotdog3.png"
@@ -192,7 +171,6 @@ export const data = [
       "id": 22,
       "category": "hotdog",
       "name": "Hotdog supremo",
-      "ingredients": ["salsicha", "pão", "molho"],
       "price": 25,
       "sale": false,
       "image": "/assets/images/hotdog4.png"
